7fc84919ba4e767d380c5be1086d46196e812258
Lucas Costa <lucascosta_s_a@hotmail.com>
2021-11-08 15:27:39 -0300

    Refactoring the Http Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -8,11 +9,12 @@ import { HomeComponent } from './pages/home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,24 +1,48 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
 import { RequestPlants } from '../interfaces/IRequests';
+import {
+  catchError,
+  map,
+  mergeMap,
+  retry,
+  switchMap,
+  tap,
+} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  getPlants(body: RequestPlants) {
-    var request = new XMLHttpRequest();
-    request.open(
-      "get",
-      `https://front-br-challenges.web.app/api/v2/green-thumb/?sun=${body.sun}&water=${body.water}&pets=${body.pets}`,
-      true
-    );
-    request.send();
+  async getPlants(body: RequestPlants) {
+    //var request = new XMLHttpRequest();
+    //http.service.spec.tsrequest.open(
+      //"get",
+      //`https://front-br-challenges.web.app/api/v2/green-thumb/?sun=${body.sun}&water=${body.water}&pets=${body.pets}`,
+      //true
+    //);
 
-    request.onload =  function () {
-      return JSON.parse(request.response);
-    };
+    //request.send();
+
+    //request.onload =  function () {
+      //return JSON.parse(request.response);
+    //};
+
+    return this.http.get(`https://front-br-challenges.web.app/api/v2/green-thumb/?sun=${body.sun}&water=${body.water}&pets=${body.pets}`).pipe(
+      retry(1),
+      map((data:any) =>{
+        return data;
+      }),
+      catchError(err => {
+        console.error('catchError', err.error);
+        return throwError(err);
+    }),
+    )
   }
 }
